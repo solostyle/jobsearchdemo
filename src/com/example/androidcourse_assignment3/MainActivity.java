48f5e62468981dd1f8b76f9feda000a223a6c3f8
solostyle <spam@solostyle.net>
2014-05-25 12:09:57 -0500

    organize imports

--- src/com/example/androidcourse_assignment3/MainActivity.java
+++ src/com/example/androidcourse_assignment3/MainActivity.java
@@ -1,8 +1,6 @@
 package com.example.androidcourse_assignment3;
 
 import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
 import java.util.List;
 
 import org.apache.http.HttpResponse;
