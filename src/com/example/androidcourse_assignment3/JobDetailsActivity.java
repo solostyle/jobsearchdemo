c50c67f9c7dc1c418836e2b617bf4ad9a414e5eb
solostyle <spam@solostyle.net>
2014-05-25 12:20:59 -0500

    job details basic info complete

--- src/com/example/androidcourse_assignment3/JobDetailsActivity.java
+++ src/com/example/androidcourse_assignment3/JobDetailsActivity.java
@@ -2,6 +2,7 @@ package com.example.androidcourse_assignment3;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class JobDetailsActivity extends Activity {
 	@Override
@@ -13,5 +14,14 @@ public class JobDetailsActivity extends Activity {
         Bundle data = getIntent().getExtras();
         Job job = (Job) data.getParcelable("JOB_DETAILS");
         setContentView(R.layout.activity_job_details);
+        
+        TextView jobTitleView = (TextView) findViewById(R.id.textView1);
+		jobTitleView.setText(job.getTitle());
+		
+		TextView companyView = (TextView) findViewById(R.id.textView2);
+		companyView.setText(job.getCompany());
+		
+		TextView locationView = (TextView) findViewById(R.id.textView3);
+		locationView.setText(job.getLocation());
     }
 }
